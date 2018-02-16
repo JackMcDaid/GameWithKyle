7db1c11e39ee0f0afdb6211bf1f6d605d88acf8e
JackMcDaid <36537860+JackMcDaid@users.noreply.github.com>
2018-02-16 08:23:57 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,14 +1,18 @@
-
+import java.util.*;
 /**
  * It's a rooom
  * 
- * @author JM & KA 
+ * @author JM and KA 
  * @version 0.01
  */
 public class Room
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String desc;
+    private String name;
+    private Arraylist<Item> roomInven = new Arraylist<Item>;
+    private int xPos;
+    private int yPos;
 
     /**
      * Constructor for objects of class Room
@@ -16,7 +20,18 @@ public class Room
     public Room()
     {
         // initialise instance variables
-        x = 0;
+        desc="";
+        name="";
+        xPos=0;
+        yPos=0;
+    }
+    public Room(String name, String desc, int xPos, int yPos, ArrayList<Item> roomInven)
+    {
+        this.name=name;
+        this.desc=desc;
+        this.xPos=xPos;
+        this.yPos=yPos;
+        this.roomInven=roomInven;
     }
 
     /**
@@ -25,9 +40,8 @@ public class Room
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public enter()
     {
-        // put your code here
-        return x + y;
+        System.out.println(desc);
     }
 }
