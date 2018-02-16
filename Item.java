448d1a68be0bd71549f059c432c15133dd044d50
JackMcDaid <36537860+JackMcDaid@users.noreply.github.com>
2018-02-16 08:14:30 -0500

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Item here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private String desc;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item()
+    {
+        // initialise instance variables
+        name="Basic";
+        desc="Basic";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
